d4abf7564805e6b58fef355419ed83d0d149c1a5
Akash03Yadav <akash.gla_mca17@gla.ac.in>
2019-03-07 11:06:52 +0530

    take textview for email and password

--- app/src/main/java/com/e/login/MainActivity.java
+++ app/src/main/java/com/e/login/MainActivity.java
@@ -2,12 +2,23 @@ package com.e.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+EditText e_mail,passw;
+Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
+
 }
